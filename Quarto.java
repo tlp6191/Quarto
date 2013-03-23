215bd02c9cc2ac61cdc23296268596758c07b9f7
TaylorPhebus <TaylorPhebus@gmail.com>
2013-03-23 12:00:57 -0500

    Corrections needed for compile

--- Quarto.java
+++ Quarto.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class Revised{
+public class Quarto{
 	public static void main(String args[]){
 		
 	}
 }
 class Game{
-	boolean player, set;
+	boolean player;//, set; set was a variable for the twist.
 	/*These are all the sets we are keeping for whether we have already solved a given state. We are mostly keeping these so we can write it out to the disk
 	We are only keeping the states with 4,8,or 12 pieces on the board
 	as a balance between the work and memory of keeping these sets, and 
@@ -21,15 +21,15 @@ class Game{
 	public static Set<Long> FourPiece=new HashSet<Long>();
 	public static Set<Long> EightPiece=new HashSet<Long>();
 	public static Set<Long> TwelvePiece=new HashSet<Long>();*/
-	public Set<byte> pieces;
+	public Set<Byte> pieces;
 	public byte[][] board;
-	//public byte chosen;
+	//public byte chosen; chosen was a variable for the twist
 	
 	Game(byte piece, byte placement, Game previous){
 		//Generates a new game state based on the placement.
 	}
 	Game(){
-		pieces=new LinkedHashSet<Long>();
+		pieces=new LinkedHashSet<Byte>();
 		board=new byte[4][4];
 	}
 	int winner(){
@@ -37,12 +37,18 @@ class Game{
 			return result();
 		}
 		int winner=-1;
-		//For every piece
+		byte nextPiece;
+		Iterator<Byte> i=pieces.iterator();
+		while(i.hasNext()){
+			nextPiece=i.next();
+			//For every piece
 			//For every placement
 			//winner=best of(winner, new Game(piece,placement,this).winner())
-			
+		}
+		return 0;
 	}
 	boolean done(){
+		return false;
 		//If out of pieces
 
 			//Return true
@@ -52,6 +58,7 @@ class Game{
 		
 	}
 	byte result(){
+		return -1;
 		//0 means tie.
 		//-1 means inconsistent gamestate (Could be useful later)
 		//1 means player 1 wins.
